a7b4df455a56b7c974669a86b9d3f66b95520e74
tnfla1130 <ksr113030@gmail.com>
2025-03-27 17:48:29 +0900

    HeeloWorld프린트문추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("이클립스 첫 실행");
 		System.out.println("hello Eclipse");
+		System.out.println("Github연동완료");
 
 	}
 
