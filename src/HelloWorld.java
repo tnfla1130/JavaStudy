12cedb9aac323b5387d239254597070e25eb2d9f
tnfla1130 <ksr113030@gmail.com>
2025-03-28 16:19:37 +0900

    집에서 작업

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,12 @@
+import ex01start.E02SystemOutPrintln;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("이클립스 첫 실행");
-		System.out.println("hello Eclipse");
-		System.out.println("Github연동완료");
-
+		System.out.println("1.이클립스 첫 실행");
+		System.out.println("2.hello Eclipse");
+		System.out.println("3.Github연동완료");
+		System.out.println("4.집에서 작업");
 	}
 
 }
